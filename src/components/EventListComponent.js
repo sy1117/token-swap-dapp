65a9ea4b24febc9e5599d69a1ad18e1093353522
김소영(So-young Kim)/솔루션개발그룹/SK <sy1117@sk.com>
2019-03-28 10:49:06 +0900

    수정 중

--- src/components/EventListComponent.js
+++ src/components/EventListComponent.js
@@ -1,4 +1,5 @@
 import React, { useMemo } from "react";
+import {Table} from 'semantic-ui-react';
 import Web3 from "web3";
 import drizzle from 'drizzle';
 import { drizzleConnect, drizzleReactHooks } from "drizzle-react";
@@ -20,10 +21,13 @@ class EventList extends React.Component{
     APContract.events.Transfer({
     }, (error, event) => { console.log(event); })
     .on('data', (event) => {
+      let logs = that.state.logs;
+      if( logs[0].transactionHash === event.transactionHash) return;
+
       this.setState({
         logs :[
           event,
-          ... that.state.logs
+          ... logs
         ] 
       })
     })
@@ -51,12 +55,27 @@ class EventList extends React.Component{
 
   render(props){
     let logs = this.state.logs.map((item,idx)=>(
-      <li key={idx}>from:{item.returnValues.from}, to:{item.returnValues.to}, value:{item.returnValues.value}</li>
+      <Table.Row>
+        <Table.Cell>{item.returnValues.from}</Table.Cell>
+        <Table.Cell>{item.returnValues.to}</Table.Cell>
+        <Table.Cell>{item.returnValues.value}</Table.Cell>
+        <Table.Cell>{item.transactionHash}</Table.Cell>
+      </Table.Row>
     ))
     return (
-      <div>
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>From</Table.HeaderCell>
+          <Table.HeaderCell>To</Table.HeaderCell>
+          <Table.HeaderCell>Value</Table.HeaderCell>
+          <Table.HeaderCell>transactionHash</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
         {logs}
-      </div>
+      </Table.Body>
+    </Table>
     )
   }
 
